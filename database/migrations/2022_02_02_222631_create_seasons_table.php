416ce2d599fea26946f93bb713c9ff8a8e6554f0
cincenfrank <fpetrangeli1@gmail.com>
2022-02-03 00:13:13 +0100

    implemented migration with foreign keys

---
+++ database/migrations/2022_02_02_222631_create_seasons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeasonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seasons', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('show_id')->constrained('shows');
+            $table->integer('ordinal_number');
+            $table->string('name')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('poster_path')->nullable();
+            $table->date('published_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seasons');
+    }
+}
